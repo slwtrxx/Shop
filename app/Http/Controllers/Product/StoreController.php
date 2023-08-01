8ad34b9588e77cdc0eeaec786989193977d4ac74
slwtrxx <140280940+slwtrxx@users.noreply.github.com>
2023-08-01 20:32:04 +0300

    Update StoreController.php

--- app/Http/Controllers/Product/StoreController.php
+++ app/Http/Controllers/Product/StoreController.php
@@ -1,19 +1,46 @@
 <?php
 
 namespace App\Http\Controllers\Product;
-use App\Models\Product;
+use App\Models\Color;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Product\StoreRequest;
+use App\Models\ColorProduct;
+use App\Models\Product;
+use App\Models\ProductTag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
     public function  __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        dd($data);
-        Product::firstOrCreate($data);
 
-       return redirect()->route('product.index');
+        $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+
+        $tagsIds = $data['tags'];
+        $colorsIds = $data['colors'];
+        unset($data['tags'], $data['colors']);
+
+        $product = Product::firstOrCreate([
+            'title'  => $data['title'],
+        ], $data);
+
+
+        foreach ($tagsIds as $tagsId)
+        ProductTag::firstOrCreate([
+            'product_id' => $product->id,
+            'tag_id' => $tagsId->id,
+        ]);
+
+        foreach ($colorsIds as $colorsId)
+            ColorProduct::firstOrCreate([
+                'product_id' => $product->id,
+                'color_id' => $colorsId,
+            ]);
+
+
+
+        return redirect()->route('product.index');
     }
 }
